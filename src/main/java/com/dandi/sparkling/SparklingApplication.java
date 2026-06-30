ec10b5f2d9d63398f3d8c9678e48a3c26d7a964b
이지원 <168502753+Vryez11@users.noreply.github.com>
2026-06-30 16:48:55 +0900

    chore: Spring Boot 프로젝트 초기 세팅
    - start.spring.io로 Mini SNS (sparkling) 골격 생성
      - Web, JPA, Security, Validation, Lombok, MySQL 의종성 추가

---
+++ src/main/java/com/dandi/sparkling/SparklingApplication.java
@@ -0,0 +1,13 @@
+package com.dandi.sparkling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparklingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SparklingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dandi/sparkling/SparklingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dandi.sparkling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SparklingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
