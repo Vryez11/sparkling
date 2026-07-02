991596207151a19699b8506e90a2d00874e5c7a6
Vryez11 <vryez11@gmail.com>
2026-07-02 19:52:25 +0900

    feat: 헬스체크 컨트롤러 Json 반환
    - String 반환에서 Json 반환으로 수정

--- src/main/java/com/dandi/sparkling/controller/HealthCheckController.java
+++ src/main/java/com/dandi/sparkling/controller/HealthCheckController.java
@@ -3,11 +3,13 @@ package com.dandi.sparkling.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 public class HealthCheckController {
 
     @GetMapping("/health")
-    public String healthCheck() {
-        return "OK";
+    public Map<String, String> healthCheck() {
+        return Map.of("status", "OK");
     }
 }
